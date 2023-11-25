12bd0d3405740c1f16f3d3d3d207d11a73bbde2b
Sergey Zuev <ekzaton@gmail.com>
2023-11-25 12:54:35 +0500

    remove hash from filenames

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      cache: true,
+      output: {
+        entryFileNames: `assets/[name].[ext]`,
+        chunkFileNames: `assets/[name].[ext]`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 })
